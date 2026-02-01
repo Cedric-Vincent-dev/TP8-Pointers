b62c9f6cd52d442e09535aa98bbf5f7662371318
Cedric-Vincent-dev <cedriclebani@gmail.com>
2026-02-01 13:06:45 +0100

    Create pointers.h

---
+++ practical/pointers.h
@@ -0,0 +1,12 @@
+#ifndef POINTERS_H
+#define POINTERS_H
+
+void demonstrateBasicIntPointer();
+void demonstrateVariousPointerTypes();
+void demonstrateConstPointerCases();
+void demonstrateStudentAllocationWithMallocFree();
+void demonstrateStudentAllocationWithNewDelete();
+void demonstratePointerArithmetic();
+void demonstratePointerDangers();
+
+#endif
